b1225e7e848821e62564feab7d79b46d5957f8aa
Oleg Mazur <mazuroleg75@gmail.com>
2022-03-17 00:08:51 +0200

    distance map

--- components/RideOptionsCard.js
+++ components/RideOptionsCard.js
@@ -1,11 +1,80 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { FlatList, SafeAreaView, StyleSheet, Text, TouchableOpacity, View } from 'react-native'
+import React, { useState } from 'react'
+import { Icon, Image } from 'react-native-elements'
+import tw from 'tailwind-react-native-classnames'
+import { useNavigation } from '@react-navigation/native'
+import { useSelector } from 'react-redux'
+import { selectTravelTimeInformation } from '../slices/navSlice'
+const data = [
+  {
+    id: "Uber-x-123",
+    title: "UberX",
+    multiplier: 1,
+    image: "http://links.papareact.com/3pn"
 
+  },
+  {
+    id: "Uber-xl-456",
+    title: "UberXL",
+    multiplier: 1.2,
+    image: "http://links.papareact.com/5w8"
+
+  },
+  {
+    id: "Uber-lux-123",
+    title: "Uber Lux",
+    multiplier: 1.75,
+    image: "http://links.papareact.com/7pf"
+
+  },
+]
 const RideOptionsCard = () => {
+  const navigation = useNavigation();
+  const [selected, setSelected]=useState(null);
+  const travelTimeInformation=useSelector(selectTravelTimeInformation);
   return (
-    <View>
-      <Text>RideOptionsCard</Text>
-    </View>
+    <SafeAreaView style={tw`bg-white flex-grow`}>
+      <View>
+        <TouchableOpacity
+          onPress={() => navigation.navigate("NavigateCard")}
+          style={tw`absolute top-3 left-5 z-50 p-3 rounded-full`}>
+          <Icon name="chevron-left" type="fontawesome" />
+        </TouchableOpacity>
+        <Text style={tw`text-center py-5 text-xl`}>Select a ride-{travelTimeInformation?.distance.text}</Text>
+      </View>
+      <FlatList
+      data={data}
+      keyExtractor={(item)=>item.id}
+      renderItem={({item:{id,title,multiplier,image},item})=>(
+        <TouchableOpacity 
+        onPress={()=>setSelected(item)}
+        style={tw`flex-row justify-between items-center px-10 
+        ${id===selected?.id&&"bg-gray-200"}`}>
+          <Image 
+          style={{
+            width:100,
+            height:100,
+            resizeMode:"contain",
+          }}
+          source={{uri:image}}/>
+          <View style={tw `-ml-6`}>
+            <Text style={tw`text-xl font-semibold`}>{title}</Text>
+            <Text>{travelTimeInformation?.duration.text}</Text>
+          </View>
+          <Text style={tw`text-xl`}>$99</Text>
+         </TouchableOpacity>
+        )}
+      />
+      <View>
+        <TouchableOpacity 
+        disabled={!selected}
+        style={tw`bg-black py-3 ${!selected && "bg-gray-300"}`}>
+          <Text style={tw`text-center text-white text-xl`}> Choose {selected?.title}</Text>
+        </TouchableOpacity>
+      </View>
+
+    </SafeAreaView>
+
   )
 }
 
